6f9eebecb73ddb9dc8591d06a336ca8ed804b1e4
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-05 22:26:45 +0300

    Initial Commit

---
+++ BadgesSTBC/BadgesSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadgesSTBCApp.swift
+//  BadgesSTBC
+//
+//  Created by Vahtee Boo on 05.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct BadgesSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
