ab68e7c0a0bb622e7deff7ce67001c184e8a41af
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-05 22:28:07 +0300

    Done

--- BadgesSTBC/ContentView.swift
+++ BadgesSTBC/ContentView.swift
@@ -2,15 +2,47 @@
 //  ContentView.swift
 //  BadgesSTBC
 //
-//  Created by Vahtee Boo on 05.04.2022.
+//  Created by Vahtee Boo on 20.03.2022.
 //
 
 import SwiftUI
 
+//use in List and TabView
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        TabView {
+            Color.red
+            tabItem {
+                Image(systemName: "heart.fill")
+                Text("Hello")
+            }
+            .badge("NEW")
+            
+            Color.green
+            tabItem {
+                Image(systemName: "heart.fill")
+                Text("Hello")
+            }
+            .badge(9)
+            
+//            Color.orange
+//            tabItem {
+//                Image(systemName: "heart.fill")
+//                Text("Hello")
+//            }
+//            .badge("NEW")
+        }
+        
+//        List {
+//            Text("All will be Ukraine!")
+//                .badge(7)
+//            Text("All will be Ukraine!")
+//                .badge("NO WAR!")
+//            Text("All will be Ukraine!")
+//            Text("All will be Ukraine!")
+//        }
     }
 }
 
